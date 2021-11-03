55d5e99f91f1e7b0e8f1d372ccbc513d3fd9c6bc
Shane McElrath <shane@rangermac.com>
2021-11-03 16:17:31 -0400

    Stoped websit for refreshing after submit.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 
-var createTaskHandler = function() {
+//Creates task and puts in in Tasks To Do section.
+var createTaskHandler = function(event) {
+    event.preventDefault();
+
     var taskItemEl = document.createElement("li");
     taskItemEl.className = "task-item";
     taskItemEl.textContent = "This is a new task.";
@@ -7,8 +10,8 @@ var createTaskHandler = function() {
 }
 
 
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
