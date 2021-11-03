4bd545066aeea7b74b5c855710cb6c690c78045f
Shane McElrath <shane@rangermac.com>
2021-11-03 13:59:43 -0400

    Added the ability to add tasks.

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+
+var createTaskHandler = function() {
+    var taskItemEl = document.createElement("li");
+    taskItemEl.className = "task-item";
+    taskItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(taskItemEl);
+}
+
+
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+
+buttonEl.addEventListener("click", createTaskHandler);
